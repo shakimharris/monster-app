7cc34cc0555cfd7b9ff3878c98d3a51a027e4b40
amenraprod <amenraprod@gmail.com>
2019-08-26 17:44:50 -0400

    rm s

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
 render() {
   return (
     <div className="App">
-      {this.state.monsters.map(monster => <h1>{monster.name}</h1>)}
+      {this.state.monster.map(monster => <h1>{monster.name}</h1>)}
     </div>
   );
  }
