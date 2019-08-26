a2759d43d7133caa1e2e938cb16a5d74017b8212
amenraprod <amenraprod@gmail.com>
2019-08-26 19:40:11 -0400

    passed props to cardlist

--- src/components/cardlist/card-list.component.jsx
+++ src/components/cardlist/card-list.component.jsx
@@ -2,5 +2,7 @@ import React from 'react';
 import './card-list.styles.css';
 
 export const CardList = props => {
-    return (<div className="card-list">{props.children}</div>)
+    return (<div className="card-list">
+    {props.monster.map(monster => (
+        <h1> {monster.name} </h1> ))}</div>)
 }
