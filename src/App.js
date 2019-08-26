567537ccfeb81ece414dce0eccc030575b67b512
amenraprod <amenraprod@gmail.com>
2019-08-26 18:41:19 -0400

    set state to fetched users

--- src/App.js
+++ src/App.js
@@ -5,23 +5,23 @@ class App extends Component {
   super();
 
   this.state = {
-    monster: [
-    ]
+    monster: []
    };
   }
+
   componentDidMount() {
    fetch('https://jsonplaceholder.typicode.com/users')
    .then(response => response.json())
-   .then(users => console.log(users))
+   .then(users => this.setState({ monster: users }));
   }
 
 render() {
   return (
     <div className="App">
-      {this.state.monster.map(monster => 
-      <h1 key={monster.id}>{monster.name}</h1>)}
-    </div>
-  );
+      {this.state.monster.map(monster => (
+      <h1> {monster.name} </h1> ))}
+  </div> 
+  ); 
  }
 }
 export default App;
