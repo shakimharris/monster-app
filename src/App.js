5ee9b86d7f0ad14a5aeff1510d17e49a9eac9b59
amenraprod <amenraprod@gmail.com>
2019-08-26 18:22:03 -0400

    grabbed users

--- src/App.js
+++ src/App.js
@@ -24,7 +24,8 @@ class App extends Component {
   }
   componentDidMount() {
    fetch('https://jsonplaceholder.typicode.com/users')
-   .then(response =>  console.log(response));
+   .then(response => response.json())
+   .then(users => console.log(users))
   }
 
 render() {
