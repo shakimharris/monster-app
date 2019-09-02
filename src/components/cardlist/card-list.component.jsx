deab739590329c8d55c18b78417fb34cd2cd6e9b
amenraprod <amenraprod@gmail.com>
2019-09-02 17:23:16 -0400

    reverted changes

--- src/components/cardlist/card-list.component.jsx
+++ src/components/cardlist/card-list.component.jsx
@@ -4,7 +4,7 @@ import { Card } from '../card/card.component';
 
 export const CardList = props => (
     <div className="card-list">
-    {props.monster.map(monster => 
+    {props.monsters.map(monster => 
     <Card key={monster.id} monster={monster} />)}
     </div>
 );
