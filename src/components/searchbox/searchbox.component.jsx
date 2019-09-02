75af0f0e8a730cf447fdf25b9f352d7740324650
amenraprod <amenraprod@gmail.com>
2019-09-02 17:22:24 -0400

    changed placeholder

--- src/components/searchbox/searchbox.component.jsx
+++ src/components/searchbox/searchbox.component.jsx
@@ -1,12 +1,14 @@
 import react from 'react';
 
-export const SearchBox = () => 
+export const SearchBox = ({ placeholder, handleChange }) => 
 return (
 <input 
     className="search-box"
     type="search" 
-    placeholder="Search" 
-    onChange={e => {
+    handleChange="Search" 
+    handleChange={e => {
     this.setState({ searchField: e.target.value }
+    }}
+    />
   );
-  }} />
+ 
