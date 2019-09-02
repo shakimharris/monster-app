09364e94a0d13dc043af4168ea4148d7066baa28
amenraprod <amenraprod@gmail.com>
2019-09-02 16:09:18 -0400

    h1 to h2

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,6 +5,6 @@ export const Card = (props) => (
 
     <div className="card-container">
         <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
-        <h1>{props.monster.name}</h1>
+        <h2>{props.monster.name}</h2>
     </div>
 );
